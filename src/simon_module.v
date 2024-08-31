a7559a04085f859e2c707730fedf80e8af1998e8
SCATE user <dont_use_this_account@example.com>
2024-08-31 19:49:23 -0400

    upload

---
+++ src/simon_module.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:14:37 11/13/2013 
+// Design Name: 
+// Module Name:    top_module 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top_module(clk,reset,data_in,data_rdy,cipher_out,valid,debug_port);
+
+input clk,data_in,reset;
+input [1:0] data_rdy;
+input debug_port;
+output reg cipher_out;
+output valid;
+
+wire key;
+wire cipher_data;
+wire [5:0] bit_counter;
+wire [6:0] round_counter;
+
+simon_datapath_shiftreg datapath(.clk(clk), 
+								 .reset(reset),
+								 .data_in(data_in), 
+								 .data_rdy(data_rdy), 
+								 .key_in(key), 
+								 .cipher_out(cipher_data), 
+								 .round_counter(round_counter), 
+								 .bit_counter(bit_counter),
+								 .valid(valid));
+											
+simon_key_expansion_shiftreg key_exp(.clk(clk), 
+									 .reset(reset), 
+									 .data_in(data_in), 
+									 .data_rdy(data_rdy), 
+									 .key_out(key), 
+									 .bit_counter(bit_counter), 
+									 .round_counter(round_counter));
+
+   always@(*)
+	 begin
+		if(debug_port==1)
+		  cipher_out = key;
+		else 
+		  cipher_out = cipher_data;
+	 end
+   
+
+endmodule
--- test/test.py
+++ test/test.py
@@ -5,7 +5,44 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+def hex_to_bits(hex_constant):
+    binary_string = bin(int(hex_constant, 16))[2:]
+    binary_string = binary_string.zfill(len(hex_constant) * 4)
+    for bit in binary_string:
+        yield int(bit)
 
+
+class ShiftRegister128:
+    def __init__(self):
+        self.register = [0] * 128
+
+    def shift_left(self, new_bit=0):
+        if new_bit not in [0, 1]:
+            raise ValueError("new_bit must be 0 or 1")
+        self.register = self.register[1:] + [new_bit]
+
+    def shift_right(self, new_bit=0):
+        if new_bit not in [0, 1]:
+            raise ValueError("new_bit must be 0 or 1")
+        self.register = [new_bit] + self.register[:-1]
+
+    def get_value(self):
+        return int("".join(map(str, self.register)), 2)
+
+    def __str__(self):
+        return "".join(map(str, self.register))
+
+store_reset_reg = ShiftRegister128()
+
+@cocotb.coroutine
+async def store_result(dut):
+    while True:
+        await RisingEdge(dut.clk)
+
+        # check if valid bit is asserted and store data output if so
+        if ((dut.uo_out >> 7) & 1):
+            store_result_reg.shift_right(dut.uo_out & 1)
+        
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
@@ -23,18 +60,61 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
+
+    # clear registers
+    # data_rdy = 3, debug_port=0, data_in=0 for 130 cycles
+    dut.ui_in = ((3 << 6) + (0 << 5) + 0)
+    await ClockCycles(dut.clk, 130)
+    
+    # data_rdy = 3, debug_port=1, data_in=0 for 130 cycles
+    dut.ui_in = ((3 << 6) + (1 << 5) + 0)
+    await ClockCycles(dut.clk, 130)
+
+    # data_rdy = 0, debug_port =0, data_in = 0 for 2 cycles
+    dut.ui_in = ((0 << 6) + (0 << 5) + 0)
+    await ClockCycles(dut.clk, 2)
+    
+    key_vectors = [ "0xd2427fba047e7fdc9fa45d04aa7a2ab7",
+                    "0x03dec884adc6ef3e1fa6bc445d5c5afe",
+                    "0x58c6c42c70324f5d60ee1efed7f0ffdf",
+                    "0xb25091ce2508d99a3d3a7f9c14223def" ]
+    
+    plain_vectors = [ "0x9f8e9892959afeeea080f1ea63e65b37",
+                      "0xd5be0328b8f87ffee3ecce3263f6ffc4",
+                      "0x49b261a460f273fce1209710987477d7",
+                      "0x4b28f378c35a5b9eac984d34c4de4d6a" ]
+    
+    cipher_vectors = [ "0xa6b6277142e0661ccd32dc221c74caf0",
+                       "0x8687e7c1beb3b9753befec46ca36b925",
+                       "0xeb1f5c3a9a583ec336fa9e136eb8e899",
+                       "0x0eb440e457fd1392f85d4b3546e99f35" ]
+    
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    for (key, pt, zt) in zip(key_vectors, plain_vectors, cipher_vectors):
+        
+        # load pt
+        for bit in hex_to_bits(pt):
+            # data_rdy = 1, debug_port = 0, data_in = bit
+            dut.ui_in = ((1 << 6) + (0 << 5) + bit)
+            await ClockCycles(dut.clk, 1)
+        
+        # load key
+        for bit in hex_to_bits(key):
+            # data_rdy = 2, debug_port = 0, data_in = bit
+            dut.ui_in = ((1 << 6) + (0 << 5) + bit)
+            await ClockCycles(dut.clk, 1)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+        # data_rdy = 0, debug_port = 0, data_in = 0
+        dut.ui_in = ((0 << 6) + (0 << 5) + 0)
+        await ClockCycles(dut.clk, 1)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        # start encryption
+        # data_rdy = 3, debug_port = 0, data_in = 0
+        dut.ui_in = ((3 << 6) + (0 << 5) + 0)
+        await ClockCycles(dut.clk, 64*71)
+        
+        assert(store_result.get_value() == int(ct, 16))
+        
+    
