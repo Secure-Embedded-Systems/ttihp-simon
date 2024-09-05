3765d7bbc44e142c1343be5bd88ee4772e6a0267
SCATE user <dont_use_this_account@example.com>
2024-09-05 08:22:34 -0400

    forced_key solution

--- test/test.py
+++ test/test.py
@@ -63,23 +63,23 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    key_vectors = [ "d2427fba047e7fdc9fa45d04aa7a2ab7", 
-                    "03dec884adc6ef3e1fa6bc445d5c5afe", 
-                    "58c6c42c70324f5d60ee1efed7f0ffdf", 
-                    "b25091ce2508d99a3d3a7f9c14223def", 
-                    "7474eaa6713849fe2d48257aa6ee77fd", 
-                    "359032b633587fbd65c2ee3c0494d7ba", 
-                    "e6b41ff02e38fd7d2ea6e78ad3382ebc", 
-                    "bc6a1fd877e2dffcdd2c015ef40afff3", 
-                    "9ffa23b6b5266eff582e20ca6af647da", 
-                    "7132fb36a99ee7fd8f0e5b42ccc4eb7f", 
-                    "e0bae594243a7da7cb26a2605bc025fa", 
-                    "2ac6813ed048ddb543a4a8307ae69bbf", 
-                    "7a74c204f2fa672edc0043f69f8cf7cf", 
-                    "3d8815c09d6afbde74d61f78698277ff", 
-                    "30b45f725b2cece7b756f4c209c05fdf", 
-                    "6c66f0f8e55077fec32c24729f8ee3de"]
-    
+    key_vectors = [ "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000",
+                    "00000000000000000000000000000000"]
+
     plain_vectors = [ "9f8e9892959afeeea080f1ea63e65b37", 
                       "d5be0328b8f87ffee3ecce3263f6ffc4", 
                       "49b261a460f273fce1209710987477d7", 
@@ -96,23 +96,24 @@ async def test_project(dut):
                       "c0c0bc124e5ee5973a64cea8ad0073ff", 
                       "960e35fac5ac74fdf87e75ec42fe4ddf", 
                       "ba5e7a389ff2ef731906f3762570becd"]
-    
-    cipher_vectors = [ "a6b6277142e0661ccd32dc221c74caf0",
-                       "8687e7c1beb3b9753befec46ca36b925",
-                       "eb1f5c3a9a583ec336fa9e136eb8e899",
-                       "0eb440e457fd1392f85d4b3546e99f35",
-                       "2dc731cceaeef69860a1c69b79f36675",
-                       "d098a10553f42e796c1f5af6f82acd10",
-                       "e432578762ddac708c0f42fa65dcf44c",
-                       "bcc1c5cf842bb24b461db48ecf307e88",
-                       "da30502f545df510ae078ee70229fd59",
-                       "1ca811e19a250aae77c828bdbf235d9e",
-                       "4a3d385fd1d6d4d51a79f3aa80f71a41",
-                       "7585fad2b65b6899ff7d5f4dcd847b14",
-                       "32a8cf4c82a798ad27f8c4fb69fd5c64",
-                       "a589e252e02c6e42904ba985307593f3",
-                       "038ccd765f330e563046e2386a02f8e5",
-                       "818a1b1154be81c393fc0371e6511ef8"]
+
+    cipher_vectors = ["e0df57e57d292d90fdbab57cfdde08d4",
+                      "d9165a86d28b9937cbd2b69142d29997",
+                      "17623a525629fcf6fb389d2e2b9632af",
+                      "600d82c9f20bf28fc204cc8e559fce6a",
+                      "124b23e763e53fd82c42b23c836f7f66",
+                      "4dd3e65c1a4045a3bd6749983ba807b1",
+                      "561479bd5de510367871dbe91be94329",
+                      "3c7fb435022b1704986b199f80aaa5ab",
+                      "9c552d1b1cba61a0c1d741a3cf75a6e4",
+                      "fe3af9194b3225a04f4183da5d7ce127",
+                      "5da257dda331bf741cea3e9ee68e22fc",
+                      "92e00628c05e5dbd137acafa4a797316",
+                      "2ece9c1e6fd8c36e0641ca0390d43566",
+                      "3746698394962e775a299e2885f14eee",
+                      "16354f36eb5fb1707d26d8a686c5c8e7",
+                      "5e15da4445d0eea480b3958cdd0030f8"]
+
     # clear registers
     # data_rdy = 3, debug_port=0, data_in=0 for 130 cycles
     dut.ui_in.value = ((3 << 6) + (0 << 5) + 0)
@@ -155,7 +156,10 @@ async def test_project(dut):
         dut.ui_in.value = ((3 << 6) + (0 << 5) + 0)
         await ClockCycles(dut.clk, 64*71)
 
-        print(hex(store_result_reg.get_value()))
+        print("Key ", key)
+        print("PT  ", pt)
+        print("CT  ", ct)
+        print("OUT ", hex(store_result_reg.get_value()))
         assert(store_result_reg.get_value() == int(ct, 16))
 
         await ClockCycles(dut.clk, 2)
